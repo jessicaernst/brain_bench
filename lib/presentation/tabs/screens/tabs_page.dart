940d795fcbcc7f1321d3e6d5a96b372cf404d539
Belladonnixi <jessica_ernst@outlook.de>
2025-02-02 16:53:02 +0100

    refactor: TabsPage and extracted bottomnavBar

--- lib/presentation/tabs/screens/tabs_page.dart
+++ lib/presentation/tabs/screens/tabs_page.dart
@@ -1,6 +1,5 @@
-import 'package:brain_bench/gen/assets.gen.dart';
+import 'package:brain_bench/presentation/tabs/widgets/brain_bench_bottom_nav_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class TabsPage extends StatefulWidget {
   const TabsPage({super.key});
@@ -12,74 +11,21 @@ class TabsPage extends StatefulWidget {
 class _TabsPageState extends State<TabsPage> {
   int _selectedIndex = 0;
 
-  Widget _buildSvgIcon({
-    required Widget Function({
-      double? width,
-      double? height,
-      ColorFilter? colorFilter,
-    }) icon,
-    Color? color,
-  }) {
-    return icon(
-      width: 24,
-      height: 24,
-      colorFilter: ColorFilter.mode(
-        color ?? Colors.white,
-        BlendMode.srcIn,
-      ),
-    );
+  //final List<Widget> _screens = [];
+
+  void _onTabSelected(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    final theme = BottomNavigationBarTheme.of(context);
-    final selectedColor = theme.selectedItemColor;
-    final unselectedColor = theme.unselectedItemColor;
-    final AppLocalizations localizations = AppLocalizations.of(context)!;
-
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
+      //body: _screens[_selectedIndex],
+      bottomNavigationBar: BrainBenchBottomNavBar(
         currentIndex: _selectedIndex,
-        onTap: (index) {
-          setState(() {
-            _selectedIndex = index;
-          });
-        },
-        items: [
-          /* BottomNavigationBarItem(
-            activeIcon: _buildSvgIcon(
-              icon: Assets.icons.homeFilled.svg,
-              color: selectedColor,
-            ),
-            icon: _buildSvgIcon(
-              icon: Assets.icons.homeOutlined.svg,
-              color: unselectedColor,
-            ),
-            label: localizations.bottomNavigationHome,
-          ), */
-          BottomNavigationBarItem(
-            activeIcon: _buildSvgIcon(
-              icon: Assets.icons.quizFilled.svg,
-              color: selectedColor,
-            ),
-            icon: _buildSvgIcon(
-              icon: Assets.icons.quizOutlined.svg,
-              color: unselectedColor,
-            ),
-            label: localizations.bottomNavigationQuiz,
-          ),
-          BottomNavigationBarItem(
-            activeIcon: _buildSvgIcon(
-              icon: Assets.icons.resultFilled.svg,
-              color: selectedColor,
-            ),
-            icon: _buildSvgIcon(
-              icon: Assets.icons.resultOulined.svg,
-              color: unselectedColor,
-            ),
-            label: localizations.bottomNavigationResults,
-          ),
-        ],
+        onTabSelected: _onTabSelected,
       ),
     );
   }
---
+++ lib/presentation/tabs/widgets/brain_bench_bottom_nav_bar.dart
@@ -0,0 +1,80 @@
+import 'package:brain_bench/gen/assets.gen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class BrainBenchBottomNavBar extends StatelessWidget {
+  final int currentIndex;
+  final ValueChanged<int> onTabSelected;
+
+  const BrainBenchBottomNavBar({
+    super.key,
+    required this.currentIndex,
+    required this.onTabSelected,
+  });
+
+  Widget _buildSvgIcon({
+    required Widget Function({
+      double? width,
+      double? height,
+      ColorFilter? colorFilter,
+    }) icon,
+    Color? color,
+  }) {
+    return icon(
+      width: 24,
+      height: 24,
+      colorFilter: ColorFilter.mode(
+        color ?? Colors.white,
+        BlendMode.srcIn,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = BottomNavigationBarTheme.of(context);
+    final selectedColor = theme.selectedItemColor;
+    final unselectedColor = theme.unselectedItemColor;
+    final AppLocalizations localizations = AppLocalizations.of(context)!;
+
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: onTabSelected,
+      items: [
+        /* BottomNavigationBarItem(
+            activeIcon: _buildSvgIcon(
+              icon: Assets.icons.homeFilled.svg,
+              color: selectedColor,
+            ),
+            icon: _buildSvgIcon(
+              icon: Assets.icons.homeOutlined.svg,
+              color: unselectedColor,
+            ),
+            label: localizations.bottomNavigationHome,
+          ), */
+        BottomNavigationBarItem(
+          activeIcon: _buildSvgIcon(
+            icon: Assets.icons.quizFilled.svg,
+            color: selectedColor,
+          ),
+          icon: _buildSvgIcon(
+            icon: Assets.icons.quizOutlined.svg,
+            color: unselectedColor,
+          ),
+          label: localizations.bottomNavigationQuiz,
+        ),
+        BottomNavigationBarItem(
+          activeIcon: _buildSvgIcon(
+            icon: Assets.icons.resultFilled.svg,
+            color: selectedColor,
+          ),
+          icon: _buildSvgIcon(
+            icon: Assets.icons.resultOulined.svg,
+            color: unselectedColor,
+          ),
+          label: localizations.bottomNavigationResults,
+        ),
+      ],
+    );
+  }
+}
