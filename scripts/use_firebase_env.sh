97e2afca1611fb863c45e2920ad0098dc0f17a8b
Belladonnixi <jessica_ernst@outlook.de>
2025-04-02 16:00:30 +0200

    refactor: update script

--- scripts/use_firebase_env.sh
+++ scripts/use_firebase_env.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-ENV=$1
+# Nimm das Environment aus dem Build Setting
+ENV="${FIREBASE_ENV}"
+
+# Fallback falls ENV leer ist
+if [ -z "$ENV" ]; then
+  echo "⚠️  No FIREBASE_ENV provided. Defaulting to 'dev'"
+  ENV="dev"
+fi
 
 echo "🛠  Applying Firebase config for environment: $ENV"
 
 # Android
-cp android/app/google-services-$ENV.json android/app/google-services.json
+cp "android/app/google-services-$ENV.json" "android/app/google-services.json"
 
 # iOS
-cp ios/Runner/GoogleService-Info-$ENV.plist ios/Runner/GoogleService-Info.plist
+cp "ios/Runner/GoogleService-Info-$ENV.plist" "ios/Runner/GoogleService-Info.plist"
+cp "ios/Runner/info-$ENV.plist" "ios/Runner/Info.plist"
 
-echo "✅ Firebase config copied for $ENV"
+echo "✅ Firebase and Info.plist copied for '$ENV'"
