c3b3e92757fe52f5fd7f57691fcc5cc792ab663f
Belladonnixi <jessica_ernst@outlook.de>
2025-05-06 17:31:04 +0200

    refactor: modularize image picker logic and add simulator support

--- lib/core/hooks/shared/use_image_picker_wrapper.dart
+++ lib/core/hooks/shared/use_image_picker_wrapper.dart
@@ -1,16 +1,16 @@
 import 'dart:io' show Platform;
-import 'package:flutter/foundation.dart' show kDebugMode;
-
-import 'package:brain_bench/core/hooks/shared/image_picker_result.dart';
 
 import 'package:brain_bench/core/hooks/prod/image_picker_impl.dart'
     if (dart.library.html) 'package:brain_bench/core/hooks/prod/image_picker_impl.dart'
     if (dart.library.io) 'package:brain_bench/core/hooks/dev_only/image_picker_impl.dart'
     as image_picker_impl;
+import 'package:brain_bench/core/hooks/shared/image_picker_result.dart';
+import 'package:flutter/foundation.dart' show kDebugMode;
 
+/// A custom hook that wraps the image picker functionality.
+/// It determines if the device is a simulator and returns the result of the image picker.
 ImagePickerResult useImagePickerWrapper() {
   final isSimulator = Platform.isIOS &&
-      !Platform.isMacOS &&
       kDebugMode &&
       Platform.environment.containsKey('SIMULATOR_DEVICE_NAME');
 
